80d92ffe2c54877508092fb24c9bc5ae89368084
kevin <kevinylee1991@gmail.com>
2024-07-01 15:59:29 -0700

    Removed some error causing lines

--- static-files/js/main.js
+++ static-files/js/main.js
@@ -4,30 +4,30 @@ function loadHTML(url, id) {
     .then((data) => {
       document.getElementById(id).innerHTML = data
 
-      if (id === "nav-placeholder") {
-        document.getElementById("spotNavbarLogo").href = window.location.origin
+      if (id === 'nav-placeholder') {
+        document.getElementById('spotNavbarLogo').href = window.location.origin
       }
     })
-    .catch((err) => console.error("Failed to load content: ", err))
+    .catch((err) => console.error('Failed to load content: ', err))
 }
 
-loadHTML("/static-files/html/nav.html", "nav-placeholder")
-loadHTML("/static-files/html/footer.html", "footer-placeholder")
+loadHTML('/static-files/html/nav.html', 'nav-placeholder')
+loadHTML('/static-files/html/footer.html', 'footer-placeholder')
 
 function toggleMobileMenu() {
-  const mobileMenu = document.getElementById("mobile-menu")
-  if (mobileMenu.style.display === "none") {
-    mobileMenu.style.display = "block"
-  } else if (mobileMenu.style.display === "") {
-    mobileMenu.style.display = "block"
+  const mobileMenu = document.getElementById('mobile-menu')
+  if (mobileMenu.style.display === 'none') {
+    mobileMenu.style.display = 'block'
+  } else if (mobileMenu.style.display === '') {
+    mobileMenu.style.display = 'block'
   } else {
-    mobileMenu.style.display = "none"
+    mobileMenu.style.display = 'none'
   }
 }
 
 async function handleLogInClick(source) {
-  gtag("event", `Clicked ${source} button`, {})
-  amplitude.track(`Clicked ${source} button`)
+  // gtag("event", `Clicked ${source} button`, {})
+  // amplitude.track(`Clicked ${source} button`)
 
   if (!lock) {
     await initializeAuth0()
@@ -36,29 +36,30 @@ async function handleLogInClick(source) {
   lock.checkSession(
     {
       redirect_uri: redirectUrl,
-      responseType: "token id_token",
+      responseType: 'token id_token',
     },
     function (err, authResult) {
       if (err) {
         lock.show()
       } else {
-        window.location.href = "/ui"
+        window.location.href = '/ui'
       }
-    }
+    },
   )
 }
 
 async function handleSignUpClick() {
-  gtag("event", "Clicked Sign Up button", {})
-  amplitude.track("Clicked Sign Up button")
+  // gtag("event", "Clicked Sign Up button", {})
+  // amplitude.track("Clicked Sign Up button")
 
   if (!lock) {
     await initializeAuth0()
   }
 
-  lock.show({ initialScreen: "signUp" })
+  lock.show({ initialScreen: 'signUp' })
 }
 
 function trackEvent(name) {
-  amplitude.track(name)
+  return
+  // amplitude.track(name)
 }
